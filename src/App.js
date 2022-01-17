bdc31750a3edf3434cdd1281ef375351f2e612a8
manuelfranco070@gmail.com <manuelfranco070@gmail.com>
2022-01-17 08:35:42 +0100

    creating the page structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Button from './components/Button';
+import Main from './components/Main';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="bg-cover bg-pink-100">
+        <Main />
+        <Button />
+      </div>
+    </>
+  );
 }
 
 export default App;
