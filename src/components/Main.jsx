2b30741ff8a8825a8feced8f65cb89167eec97d0
manuelfranco070@gmail.com <manuelfranco070@gmail.com>
2022-01-18 11:53:45 +0100

    some changes on container class

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,22 +17,24 @@ const Main = () => {
           <h1 className="text-[24px] font-bold tracking-wide text-center">
             Channel name
           </h1>
-          <p className="my- bg-[#f5eaf5] text-base font-normal tracking-wide text-center ml-20 mr-20 rounded">
+          <p className="my- bg-[#f5eaf5] text-base font-normal tracking-wide text-center mt-3 mb-7 ml-20 mr-20 rounded">
             46K Active users・78K Total users
           </p>
         </div>
-        <form>
-          <h1>User name</h1>
-          <input
-            type="text"
-            placeholder="How do you like people to call you?"
-          />
-        </form>
-        <div>
-          <input type="checkbox" />
-          <p>
-            I have read and agree to the Terms of service and Privacy policy.
-          </p>
+        <div className="md:container mx-auto px-12">
+          <form className="">
+            <h1 className="">User name</h1>
+            <input
+              type="text"
+              placeholder="How do you like people to call you?"
+            />
+          </form>
+          <div>
+            <input type="checkbox" />
+            <p>
+              I have read and agree to the Terms of service and Privacy policy.
+            </p>
+          </div>
         </div>
       </section>
     </>
